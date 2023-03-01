bddbe7f5361c52af84d57a62eb9422b49bd38e22
Farhan Mahmood <113241435+farhanhere@users.noreply.github.com>
2023-03-02 01:02:06 +0500

    Second commit

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
-import logo from './assets/logo.png'
+import{styles} from "./cssStyle";
+import logo from './assets/logo.png';
 export default function App() {
   return (
     <View style={styles.Login}>
@@ -25,84 +26,3 @@ export default function App() {
 
   );
 }
-
-const styles = StyleSheet.create({
-  Login: {
-    flex: 1,
-    backgroundColor: 'green',
-
-  },
-  Logo: {
-    flex: 0.3,
-    backgroundColor: 'black',
-
-  },
-  feild: {
-    flex: 0.4,
-    backgroundColor: 'black',
-
-  },
-  button: {
-    flex: 0.3,
-    backgroundColor: 'black',
-
-  },
-  logoimage: {
-    width: 130,
-    height: 130,
-    marginTop: 80,
-    marginLeft: 130,
-  },
-  text1: {
-    width: 300,
-    height: 40,
-    margin: 12,
-    backgroundColor: "#fff",
-    marginLeft: 50,
-    marginTop: 100,
-    paddingLeft: 10,
-    borderRadius:5
-  },
-  text2: {
-    width: 300,
-    height: 40,
-    backgroundColor: "#fff",
-    marginLeft: 50,
-    marginTop: 10,
-    paddingLeft: 10,
-    borderRadius:5
-  },
-  loginbutton: {
-    width: 150,
-    height:40,
-    marginLeft: 120,
-    marginTop: 0,
-    backgroundColor: 'blue',
-    display: 'flex',
-    justifyContent:'center',
-    alignItems: 'center',
-    borderRadius:5
-  },
-  buttontext:{
-    fontWeight:'bold',
-    color:'#fff',
-    fontSize:18,
-  },
-  signupbutton: {
-    width: 150,
-    height:40,
-    marginLeft: 120,
-    marginTop: 20,
-    backgroundColor: 'blue',
-    display: 'flex',
-    justifyContent:'center',
-    alignItems: 'center',
-    borderRadius:5
-  },
-  forget:{
-    color: '#fff',
-    marginLeft:235,
-    marginTop:10,
-  }
- 
-});
---
+++ cssStyle.js
@@ -0,0 +1,85 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    Login: {
+      flex: 1,
+      backgroundColor: 'green',
+  
+    },
+    Logo: {
+      flex: 0.3,
+      backgroundColor: 'black',
+  
+    },
+    feild: {
+      flex: 0.4,
+      backgroundColor: 'black',
+  
+    },
+    button: {
+      flex: 0.3,
+      backgroundColor: 'black',
+  
+    },
+    logoimage: {
+      width: 130,
+      height: 130,
+      marginTop: 80,
+      marginLeft: 130,
+    },
+    text1: {
+      width: 300,
+      height: 40,
+      margin: 12,
+      backgroundColor: "#fff",
+      marginLeft: 50,
+      marginTop: 100,
+      paddingLeft: 10,
+      borderRadius:5
+    },
+    text2: {
+      width: 300,
+      height: 40,
+      backgroundColor: "#fff",
+      marginLeft: 50,
+      marginTop: 10,
+      paddingLeft: 10,
+      borderRadius:5
+    },
+    loginbutton: {
+      width: 150,
+      height:40,
+      marginLeft: 120,
+      marginTop: 0,
+      backgroundColor: 'blue',
+      display: 'flex',
+      justifyContent:'center',
+      alignItems: 'center',
+      borderRadius:5
+    },
+    buttontext:{
+      fontWeight:'bold',
+      color:'#fff',
+      fontSize:18,
+    },
+    signupbutton: {
+      width: 150,
+      height:40,
+      marginLeft: 120,
+      marginTop: 20,
+      backgroundColor: 'blue',
+      display: 'flex',
+      justifyContent:'center',
+      alignItems: 'center',
+      borderRadius:5
+    },
+    forget:{
+      color: '#fff',
+      marginLeft:235,
+      marginTop:10,
+    }
+   
+  });
+
+  export {styles}
+  
